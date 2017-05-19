a49c316e68eb4dfb939c6ada5d4f518d2824fafa
mightyplow <mightyplow@gmail.com>
2017-05-20 00:10:58 +0200

    cache on tag creation; added preconnect type

--- src/prefetcher.js
+++ src/prefetcher.js
@@ -1,30 +1,36 @@
-const createElement = document.createElement.bind(document);
-
 const loaded = [];
 
 const types = {
     PREFETCH: 'prefetch',
     PRELOAD: 'preload',
     PRERENDER: 'prerender',
-    DNS_PREFETCH: 'dns-prefetch'
+    DNS_PREFETCH: 'dns-prefetch',
+    PRECONNECT: 'preconnect'
 };
 
 const createLink = (rel, href) => {
-    const link = createElement('link');
+    const link = document.createElement('link');
     Object.assign(link, {rel, href});
     return link;
 };
 
-const removeOnLoad = src => ({ target }) => {
+const removeOnLoad = ({ type, target }) => {
     target.parentNode.removeChild(target);
-    loaded.push(src);
+
+    if (type === 'error') {
+        console.info(`'${target.href}' could not be prefetched`);
+    }
 };
 
 const createPrefetchLink = (type, src) => {
     if (loaded.indexOf(src) < 0) {
-        const link = createLink('prefetch', src);
-        link.addEventListener('load', removeOnLoad(src));
+        const link = createLink(type, src);
+        link.addEventListener('load', removeOnLoad);
+        link.addEventListener('error', removeOnLoad);
         document.head.appendChild(link);
+
+        // move it to the list of already loaded sources
+        loaded.push(src);
     }
 };
 
@@ -32,10 +38,12 @@ export const prefetch = createPrefetchLink.bind(null, types.PREFETCH);
 export const preload = createPrefetchLink.bind(null, types.PRELOAD);
 export const prerender = createPrefetchLink.bind(null, types.PRERENDER);
 export const prefetchDns = createPrefetchLink.bind(null, types.DNS_PREFETCH);
+export const preconnect = createPrefetchLink.bind(null, types.PRECONNECT);
 
 export default {
     prefetch,
     preload,
     prerender,
-    prefetchDns
+    prefetchDns,
+    preconnect
 };
